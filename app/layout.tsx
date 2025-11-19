90c7dd547bae471c90c48113039eea0033848109
riownl15-cmd <riownl15@gmail.com>
2025-11-19 19:34:01 +0530

    Updated globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className="bg-white">
+      <body className={`${inter.className} bg-white`}>
         <AuthProvider>
           {children}
           <Toaster />
